c61c80025b7eea76ec5e9e75fb8abd828a2ef6fb
AVINASH RAJ <mrhecker0508@gmail.com>
2025-05-25 12:37:06 +0530

    Added the different pages in "component" folder created "Dashboard.js" file Added routings to navigate betweeen pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
-import LoginForm from "./LoginForm";
+import LoginForm from "./components/LoginForm";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from "./components/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function Home() {
+  return <div>Home Page</div>;
+}
 
 function App() {
   return (
-    <LoginForm/>
+    <Router>
+      <Routes>
+        <Route path="/z_coder/" element={<LoginForm />} />
+        <Route path="/z_coder/login" element={<LoginForm />} />
+        <Route
+          path="/z_coder/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<div>404 Not Found</div>} />
+
+      </Routes>
+    </Router>
   );
 }
 
